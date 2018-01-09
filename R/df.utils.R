b279042a27eb1ad25d02164912c902cde74f314d
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2018-01-09 15:54:40 +0100

    Add dist.gc()
    to calculate great-circle distances based on the haversin formula

--- R/df.utils.R
+++ R/df.utils.R
@@ -155,3 +155,32 @@ remap <- function(df.src, df.dest) {
         tidyr::spread(plotutils_remap_variables_key,
                       plotutils_remap_variables_value)
 }
+
+#' Great-circle distances
+#'
+#' Calculate great-circle distances based on the haversin formula
+#'
+#' @param lon1 Vector of longitudes (in degrees) 
+#' @param lon2 Vector of longitudes (in degrees) 
+#' @param lat1 Vector of latitudes (in degrees) 
+#' @param lat2 Vector of latitudes (in degrees)
+#' @return Vector of distances (in km) between points
+#'
+#' @export
+#'
+#' @examples
+#' 
+dist.gc <- function(lon1, lon2, lat1, lat2) {
+    haversin <- function(theta) sin(0.5 * theta)^2
+    re <- 6371
+
+    lon1 <- lon1 / 180 * pi
+    lon2 <- lon2 / 180 * pi
+    lat1 <- lat1 / 180 * pi
+    lat2 <- lat2 / 180 * pi
+
+    theta <- sqrt(haversin(lat1 - lat2) +
+                  cos(lat1) * cos(lat2) * haversin(lon1 - lon2))
+    dist <- 2 * re * asin(pmin(pmax(theta, 0), 1))
+    dist
+}
